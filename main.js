33dedd059de5fa34e443dbb1e24798e309f50b0b
Victor <victor.clarke@alphamaintenance-sa.fr>
2022-05-09 11:32:30 +0200

    end

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-import axios from "axios";
+const api = "https://chympy.net/api/articles/";
+const errors = document.querySelector('.errors');
+const data = document.querySelector('.data');
+const form = document.querySelector('.form-data');
 
-const api = "https://chympy.net/api/articles";
-const loading = document.querySelector(".loading");
-const errors = document.querySelector(".errors");
-const data = document.querySelector(".data");
+const  searchForArticle = async ()  => {
+    errors.textContent  = '';
+    try {
+        let myHeaders = new Headers();
+
+        httpGetAsync(api,function (e) {
+
+            let arr = JSON.parse(e);
+            for (let i = 0 ; i < arr.length ; i++ ){
+                data.innerHTML += `<h2> ${arr[i].Title}</h2>`
+            }
+        })
+
+    }catch (error){
+        console.log(error);
+        errors.textContent = error.toString();
+    }
+}
+window.addEventListener('load', searchForArticle)
+
+function httpGetAsync(theUrl, callback)
+{
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
